6afbfe190b5ed4ba2d50afcc04468e8170ed454e
Emmanuelavn <avannabenito34@gmail.com>
2026-05-21 01:39:55 +0200

    id

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Je Prie Tout Seul — 30 Prières du Soir pour Enfants | ELYX",
@@ -30,6 +31,31 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="font-body antialiased">{children}</body>
+
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1534531525046116');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1534531525046116&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
     </html>
   );
 }
